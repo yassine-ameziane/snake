cce8cec82dd434e49be444ca7f59d3d00b0860c4
yassine-ameziane <y.ameziane@hotmail.com>
2022-10-18 15:04:34 +0200

    1

---
+++ 1A yassine.R
@@ -0,0 +1,30 @@
+for(pkg in c("AER", "lmtest", "Hmisc", "dplyr", "tibble", "ivreg", "modelsummary",
+"summarytools")){
+  if(!require(pkg, character.only = TRUE))
+  {
+    install.packages(pkg, repos = 'http://cloud.r-project.org')
+    library(pkg, character.only = TRUE)
+  }
+}
+
+library('skimr')
+library('psych')
+
+fertility = read.csv("fertility.csv")
+View(fertility)
+
+# colMeans(fertility)
+# length(fertility[,'educ'])
+
+description = describe(fertility)
+description
+describe(fertility)$n[c(1,4:6)]
+describe(fertility)$mean[c(1,4:6)]
+describe(fertility)$sd[c(1,4:6)]
+describe(fertility)$min[c(1,4:6)]
+describe(fertility)$max[c(1,4:6)]
+
+summary(fertility)
+skim(fertility)
+
+
