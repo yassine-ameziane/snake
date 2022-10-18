0c14307e5dbc063ba9c2fffbfecbf7a98ce77c37
20214247 <i.ljamai@student.tue.nl>
2022-10-18 15:33:47 +0200

    SYT

--- 1B imran.R
+++ 1B imran.R
@@ -1,1 +1,3 @@
-groupedby_fertility = fertility %>% group_by(year) %>% summarise(mean_kids = mean(kids))
+groupedby_fertility = fertility %>% group_by(year) %>% summarise(mean_number_of_children = mean(kids))
+plot(groupedby_fertility ,main="Mean number of children between 2002-2014", xlab="Years",
+ylab="Mean number of children")
