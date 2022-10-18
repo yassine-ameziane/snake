75558aa161b85326aebca3767f8db01428cc8163
yassine-ameziane <y.ameziane@hotmail.com>
2022-10-18 15:36:07 +0200

    2

--- 1B imran.R
+++
@@ -1,3 +0,0 @@
-groupedby_fertility = fertility %>% group_by(year) %>% summarise(mean_number_of_children = mean(kids))
-plot(groupedby_fertility ,main="Mean number of children between 2002-2014", xlab="Years",
-ylab="Mean number of children")
